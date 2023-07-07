d327fdd0e39ece35fd0e844c31826e75ad38f41c
Cavaire3D <119263049+Cavaire3D@users.noreply.github.com>
2023-07-07 12:00:13 -0500

    temp edit

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
 import 'dotenv/config';
 import './src/commands/games/2048.js';
+const Discord = require("discord.js");
+const { GatewayIntentBits } = require("discord.js");
+const client = new Discord.Client({
+  intents: [
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+const config = require("./config.json");
+const database = require('easy-json-database')
+const db = new database("./FishyData/PlayerDatabase.json", {
+  snapshots: {
+      enabled: false
+  }
+});
+const { eventLoader, commandLoader } = require("./src/functions/loaders");
+client.commands = new Discord.Collection();
+client.slashCommands = [];
+client.database = db;
+  
+eventLoader(client);
+commandLoader(client);
 
+client.login(config.token);
 import express from 'express';
 import {
   InteractionType,
