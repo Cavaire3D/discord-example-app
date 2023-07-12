e6e14116d7dc5ab6b212b213817548be9c7db630
Cavaire3D <119263049+Cavaire3D@users.noreply.github.com>
2023-07-11 21:53:33 -0500

    Fix update

--- commands/economy/mining.js
+++ commands/economy/mining.js
@@ -11,5 +11,5 @@ $description[$random[0;5] Diamond💎!, \`$getServerVar[prefix]selldiamond\` to 
 $globalCooldown[2s;**⏰ Wait for %time% to mining again!**]
 $onlyIf[$getGlobalUserVar[hungry]>=10;Your hunger rate is at 10%, and you should eat first! You can buy the food.]
 $onlyIf[$getGlobalUserVar[thirsty]>=20;Your thirst level is at 20%, and you should drink first! You can buy the drink.]
-$onlyIf[$getGlobalUserVar[health]>=30;Your health rate is at 30%, and you should take and take medicine first! You can buy it.]`
+$onlyIf[$getGlobalUserVar[health]>=30;Your health rate is at 30%, and you should take medicine first! You can buy it.]`
 }
